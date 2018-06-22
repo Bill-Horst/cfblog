8c0b5bfea6b43993d85917b5aecc3f2533e84536
Bill Horst <bill@billhorst.com>
2018-06-22 13:48:44 -0600

    trying out wysihtml5-rails text editor

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ end
 gem 'devise'
 
 gem 'cancancan', '~> 1.10'
+
+gem 'bootstrap-wysihtml5-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,11 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery3
+//= require bootstrap-wysihtml5
+//= require bootstrap-wysihtml5/locales
+//= require bootstrap-wysihtml5/wysihtml5x-toolbar.js
+//= require bootstrap-wysihtml5/handlebars.runtime.min.js
+
+//= require bootstrap-wysihtml5/minimum
+
 //= require_tree .
